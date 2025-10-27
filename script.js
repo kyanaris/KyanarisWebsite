b5f5eb261c98b4177178fcdf6dd8ae7463471fc0
Kyanaris <kyanarisofficial@gmail.com>
2025-10-27 23:37:20 +0400

    Release files

---
+++ script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('modal');
+const openModalBtn = document.getElementById('openModal');
+const closeModalBtn = document.getElementById('closeModal');
+
+openModalBtn.addEventListener('click', () => {
+  modal.classList.add('active');
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modal.classList.remove('active');
+});
+
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.classList.remove('active');
+  }
+});
